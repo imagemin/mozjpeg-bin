694c06b42ebc745838a9af07b6bde34b9156a94f
Kevin Martensson <kevinmartensson@gmail.com>
2014-05-13 10:47:20 +0200

    Use bin-wrapper 0.3.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var BinBuild = require('bin-build');
 var BinWrapper = require('bin-wrapper');
 var chalk = require('chalk');
 var fs = require('fs');
@@ -9,25 +10,28 @@ var path = require('path');
  * Initialize a new BinWrapper
  */
 
-var bin = new BinWrapper({ bin: 'jpegtran', dest: path.join(__dirname, 'vendor') });
-var bs = 'autoreconf -fiv && ./configure --prefix="' + bin.dest +
-         '" --bindir="' + bin.dest + '" --libdir="' + bin.dest + '" && make && make install';
+var bin = new BinWrapper()
+    .dest(path.join(__dirname, 'vendor'))
+    .use('jpegtran');
 
 /**
  * Only run check if binary doesn't already exist
  */
 
-fs.exists(bin.path, function (exists) {
+fs.exists(bin.use(), function (exists) {
     if (!exists) {
-        bin
-            .addSource('https://github.com/mozilla/mozjpeg/archive/v1.0.1.tar.gz')
-            .build(bs)
-            .on('error', function (err) {
-                return console.log(chalk.red('✗ ' + err.message));
-            })
-            .on('finish', function () {
-                return console.log(chalk.green('✓ mozjpeg rebuilt successfully'));
-            });
+        var builder = new BinBuild()
+            .src('https://github.com/mozilla/mozjpeg/archive/v1.0.1.tar.gz')
+            .cfg('autoreconf -fiv && ./configure --prefix="' + bin.dest() + '" --bindir="' + bin.dest() + '" --libdir="' + bin.dest() + '"')
+            .make('make && make install');
+
+        return builder.build(function (err) {
+            if (err) {
+                return console.log(chalk.red('✗ ' + err));
+            }
+
+            console.log(chalk.green('✓ mozjpeg built successfully'));
+        });
     }
 });
 
@@ -35,4 +39,4 @@ fs.exists(bin.path, function (exists) {
  * Module exports
  */
 
-module.exports.path = bin.path;
+module.exports.path = bin.use();
--- test/test.js
+++ test/test.js
@@ -3,32 +3,33 @@
 
 var assert = require('assert');
 var binCheck = require('bin-check');
-var BinWrapper = require('bin-wrapper');
+var BinBuild = require('bin-build');
+var execFile = require('child_process').execFile;
 var fs = require('fs');
 var path = require('path');
-var spawn = require('child_process').spawn;
 var rm = require('rimraf');
 
-describe('optipng()', function () {
+describe('mozjpeg()', function () {
     afterEach(function (cb) {
         rm(path.join(__dirname, 'tmp'), cb);
     });
 
-    beforeEach(function (cb) {
-        fs.mkdir(path.join(__dirname, 'tmp'), cb);
+    beforeEach(function () {
+        fs.mkdirSync(path.join(__dirname, 'tmp'));
     });
 
     it('should rebuild the mozjpeg binaries', function (cb) {
-        var bin = new BinWrapper({ bin: 'jpegtran', dest: path.join(__dirname, 'tmp') });
-        var bs = 'autoreconf -fiv && ./configure --prefix="' + bin.dest +
-                 '" --bindir="' + bin.dest + '" --libdir="' + bin.dest + '" && make && make install';
+        var tmp = path.join(__dirname, 'tmp');
+        var builder = new BinBuild()
+            .src('https://github.com/mozilla/mozjpeg/archive/v1.0.1.tar.gz')
+            .cfg('autoreconf -fiv && ./configure --prefix="' + tmp + '" --bindir="' + tmp + '" --libdir="' + tmp + '"')
+            .make('make && make install');
 
-        bin
-            .addSource('https://github.com/mozilla/mozjpeg/archive/v1.0.1.tar.gz')
-            .build(bs)
-            .on('finish', function () {
-                cb(assert(true));
-            });
+        builder.build(function (err) {
+            assert(!err);
+            assert(fs.existsSync(path.join(tmp, 'mozjpeg')));
+            cb();
+        });
     });
 
     it('should return path to binary and verify that it is working', function (cb) {
@@ -43,20 +44,18 @@ describe('optipng()', function () {
         });
     });
 
-    it('should minify a JPEG', function (cb) {
+    it('should minify a JPG', function (cb) {
         var binPath = require('../').path;
         var args = [
             '-outfile', path.join(__dirname, 'tmp/test.jpg'),
             path.join(__dirname, 'fixtures', 'test.jpg')
         ];
 
-        spawn(binPath, args).on('close', function () {
+        execFile(binPath, args, function () {
             var src = fs.statSync(path.join(__dirname, 'fixtures/test.jpg')).size;
             var dest = fs.statSync(path.join(__dirname, 'tmp/test.jpg')).size;
 
-            assert(dest < src);
-            assert(dest > 0);
-            cb();
+            cb(assert(dest < src));
         });
     });
 });
