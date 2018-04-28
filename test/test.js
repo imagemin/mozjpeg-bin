a46838a183ca6ec46a41f58608c3b75e919c34a9
Sindre Sorhus <sindresorhus@gmail.com>
2018-04-28 20:25:52 +0700

    Improve tests

--- test/test.js
+++ test/test.js
@@ -12,22 +12,21 @@ const mozjpeg = require('..');
 
 const cpuNum = os.cpus().length;
 
-test.cb('rebuild the mozjpeg binaries', t => {
+test('rebuild the mozjpeg binaries', async t => {
 	const tmp = tempy.directory();
 	const cfg = [
 		'./configure --disable-shared --disable-dependency-tracking --with-jpeg8',
 		`--prefix="${tmp}" --bindir="${tmp}" --libdir="${tmp}"`
 	].join(' ');
 
-	binBuild.url('https://github.com/mozilla/mozjpeg/releases/download/v3.2/mozjpeg-3.2-release-source.tar.gz', [
+	await binBuild.url('https://github.com/mozilla/mozjpeg/releases/download/v3.2/mozjpeg-3.2-release-source.tar.gz', [
 		'autoreconf -fiv',
 		cfg,
 		`make --jobs=${cpuNum}`,
 		`make install --jobs=${cpuNum}`
-	]).then(() => {
-		t.true(fs.existsSync(path.join(tmp, 'cjpeg')));
-		t.end();
-	});
+	]);
+
+	t.true(fs.existsSync(path.join(tmp, 'cjpeg')));
 });
 
 test('return path to binary and verify that it is working', async t => {
