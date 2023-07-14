1dc4f16a0d2d94d714e2fa271f0217a851181a1b
Mateusz Krawczuk <mat.krawczuk@gmail.com>
2023-07-14 11:18:11 +0200

    Add paths for cjpeg for 32-bit and 64-bit windows

--- test/test.js
+++ test/test.js
@@ -3,22 +3,22 @@ import path from 'node:path';
 import process from 'node:process';
 import {fileURLToPath} from 'node:url';
 import test from 'ava';
-import execa from 'execa';
-import tempy from 'tempy';
+import {execa} from 'execa';
+import {temporaryDirectory} from 'tempy';
 import binCheck from 'bin-check';
 import binBuild from 'bin-build';
 import compareSize from 'compare-size';
 import mozjpeg from '../index.js';
 
 test('rebuild the mozjpeg binaries', async t => {
+	const temporary = temporaryDirectory();
+	const config = [];
 	// Skip the test on Windows
 	if (process.platform === 'win32') {
 		t.pass();
 		return;
 	}
 
-	const temporary = tempy.directory();
-	const config = [];
 	if (process.platform === 'darwin') {
 		config.push('-DCMAKE_FIND_FRAMEWORK=LAST -DBUILD_SHARED_LIBS=OFF');
 	}
@@ -42,8 +42,8 @@ test('return path to binary and verify that it is working', async t => {
 });
 
 test('minify a JPG', async t => {
-	const temporary = tempy.directory();
-	const src = fileURLToPath(new URL('./fixtures/test.jpg', import.meta.url));
+	const temporary = temporaryDirectory();
+	const src = fileURLToPath(new URL('fixtures/test.jpg', import.meta.url));
 	const dest = path.join(temporary, 'test.jpg');
 	const args = [
 		'-outfile',
