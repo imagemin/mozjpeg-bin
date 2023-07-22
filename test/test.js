c0587fbc00b21ed8cad8bae499a0827baeaf7ffa
Mateusz Krawczuk <2733498+MateuszKrawczuk@users.noreply.github.com>
2023-07-22 10:53:29 +0200

    Add support for arm64 and update mozjpeg 4.1.3 (#80)
    * Add support for arm64 and update mozjpeg 4.1.3
    
    Windows building is broken. It has to be fixed. I recommend using vcpkg.
    Mac Os X arm64 binary should be change to real arm64 binary. I don't own mac, but everything builds correctly.
    
    * Add paths for cjpeg for 32-bit and 64-bit windows

--- test/test.js
+++ test/test.js
@@ -1,40 +1,40 @@
 import fs from 'node:fs';
-import os from 'node:os';
 import path from 'node:path';
 import process from 'node:process';
 import {fileURLToPath} from 'node:url';
 import test from 'ava';
-import execa from 'execa';
-import tempy from 'tempy';
+import {execa} from 'execa';
+import {temporaryDirectory} from 'tempy';
 import binCheck from 'bin-check';
 import binBuild from 'bin-build';
 import compareSize from 'compare-size';
 import mozjpeg from '../index.js';
 
-const cpuNumber = os.cpus().length;
-
 test('rebuild the mozjpeg binaries', async t => {
+	const temporary = temporaryDirectory();
+	const config = [];
 	// Skip the test on Windows
 	if (process.platform === 'win32') {
 		t.pass();
 		return;
 	}
 
-	const temporary = tempy.directory();
+	if (process.platform === 'darwin') {
+		config.push('-DCMAKE_FIND_FRAMEWORK=LAST -DBUILD_SHARED_LIBS=OFF');
+	}
+
 	const cfg = [
-		'./configure --enable-static --disable-shared --disable-dependency-tracking --with-jpeg8',
-		`--prefix="${temporary}" --bindir="${temporary}" --libdir="${temporary}"`,
+		`cmake  ${config.join(' ')} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${temporary}" .`,
 	].join(' ');
 
 	const source = fileURLToPath(new URL('../vendor/source/mozjpeg.tar.gz', import.meta.url));
 	await binBuild.file(source, [
-		'autoreconf -fiv',
 		cfg,
-		`make --jobs=${cpuNumber}`,
-		`make install --jobs=${cpuNumber}`,
+		'cmake --build . ',
+		'cmake --install . ',
 	]);
 
-	t.true(fs.existsSync(path.join(temporary, 'cjpeg')));
+	t.true(fs.existsSync(path.join(temporary, 'bin/cjpeg')));
 });
 
 test('return path to binary and verify that it is working', async t => {
@@ -42,8 +42,8 @@ test('return path to binary and verify that it is working', async t => {
 });
 
 test('minify a JPG', async t => {
-	const temporary = tempy.directory();
-	const src = fileURLToPath(new URL('./fixtures/test.jpg', import.meta.url));
+	const temporary = temporaryDirectory();
+	const src = fileURLToPath(new URL('fixtures/test.jpg', import.meta.url));
 	const dest = path.join(temporary, 'test.jpg');
 	const args = [
 		'-outfile',
